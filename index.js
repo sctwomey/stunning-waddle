835f783b75b21557ca202f09fbabd1ce09b11e72
sctwomey <sctwomey@gmail.com>
2023-09-30 17:29:55 -0700

    Add questions and write function to js file

--- index.js
+++ index.js
@@ -1,16 +1,79 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application.
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input, and the write function for creating the README file.
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your GitHub profile username?",
+        name: "github",
+        default: "None"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+        default: "None"
+    },
+    {
+        type: "input",
+        message: "What is the title of your project? (Required)",
+        name: "title",
+    },
+    {
+        type: "editor",
+        message: "Describe your project.",
+        name: "description",
+        default: "No description provided."
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions for your project?",
+        name: "installation",
+        default: "No installation instructions provided."
+    },
+    {
+        type: "input",
+        message: "How do you use your project?",
+        name: "usage",
+        default: "No usage information provided."
+    },
+    {
+        type: "list",
+        message: "Please choose a license for your project.",
+        name: "license",
+        choices: [
+            'AFL-3.0',
+            'APACHE-2.0',
+            'BSL-1.0',
+            'CC',
+            'CC-BY-4.0',
+            'MIT',
+            'MPL-2.0',
+            'PostgreSQL'
+        ],
+    },
+    {
+        type: "input",
+        message: "What type of contributions will be allowed for your project?",
+        name: "contributing",
+        default: "No contribution information provided."
+    },
+    {
+        type: "editor",
+        message: "Are there any test instructions for your project?",
+        name: "tests",
+        default: "No test information provided."
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+])
+    .then((data) => {
+        console.log(data);
 
-// TODO: Create a function to initialize app
-function init() { }
+        fs.writeFile("README.md", generateMarkdown(data), (error) => {
+            error ? console.log(error) : console.log("Success!");
+        });
 
-// Function call to initialize app
-init();
+    });
