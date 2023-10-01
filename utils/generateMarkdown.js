505d409eb446749c02dae0aa1517495e3379a8ba
sctwomey <sctwomey@gmail.com>
2023-09-30 17:31:35 -0700

    Add template literal to js file for README structure

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,1 +1,41 @@
+// This function generates the markdown structure for the README file that is created after all of the questions are answered. 
+// Note: The starter functions were removed since no other functions are required to generate the structure for the README file.
 
+function generateMarkdown(data) {
+    return `
+    # ${data.title} ![License](https://img.shields.io/badge/License-${data.license}-blue.svg)
+
+  ## Description
+    ${data.description}
+  
+  ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+  
+  
+  ### Installation
+    ${data.installation}
+  
+  ### Usage
+    ${data.usage}
+  
+  ### License
+    ${data.license}
+  
+  ### Contributing
+    ${data.contributing}
+  
+  ### Tests
+    ${data.tests}
+  
+  ### Questions
+    If there are any questions regarding this project, you may contact me at 
+    ${data.email}, or by visiting ${data.github}.   
+    `;
+};
+
+module.exports = generateMarkdown;
