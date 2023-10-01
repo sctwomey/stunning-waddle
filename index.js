d1754034660ee09b3107e6432a117585799246ef
sctwomey <sctwomey@gmail.com>
2023-10-01 01:53:42 -0700

    Update regex for email validation in js file

--- index.js
+++ index.js
@@ -97,10 +97,10 @@ function inputValidation(data) {
     return true;
 };
 
-// Validation for user email (regex from https://masteringjs.io/tutorials/fundamentals/email-regex).
+// Validation for user email being RFC 5322 compliant (regex from https://regex101.com/r/3uvtNl/1).
 function emailValidation(data) {
 
-    const emailPattern = /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+    const emailPattern = /^((?:[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]|(?<=^|\.)"|"(?=$|\.|@)|(?<=".*)[ .](?=.*")|(?<!\.)\.){1,64})(@)((?:[A-Za-z0-9.\-])*(?:[A-Za-z0-9])\.(?:[A-Za-z0-9]){2,})$/gm;
 
     if (!emailPattern.test(data)) {
         return 'Please enter a valid email!';
